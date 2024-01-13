573d187fd43a1b86e07d5b24bf4c9acb9c03f20d
CoolOmer <127267988+CoolOmer@users.noreply.github.com>
2024-01-13 16:03:19 -0500

    Update omer_lcd_weather.ino

--- omer_lcd_weather.ino
+++ omer_lcd_weather.ino
@@ -22,7 +22,7 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
-//testing?
+//Defining gthe LCD1602's associated pins and connections.
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
